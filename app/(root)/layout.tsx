bcfaeea796c24a0dd88cadf15005aa92e629da9e
mmxfxqr <holodov.sas@yandex.ru>
2024-09-29 10:15:21 +0300

    add new layout for all my app and changing rootLayout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,6 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
-import "../globals.css";
 import { Header } from "@/components/shared/header";
-
-const nunito = Nunito({
-  subsets: ["cyrillic"],
-  variable: "--font-nunito",
-  weight: ["400", "500", "600", "700", "800", "900"],
-});
 
 export const metadata: Metadata = {
   title: "Next Pizza",
@@ -19,12 +12,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={nunito.variable}>
-        <main className="min-h-screen">
-          <Header/>
-          {children}</main>
-      </body>
-    </html>
+    <main className="min-h-screen">
+      <Header />
+      {children}
+    </main>
   );
 }
--- components/shared/products-group-list.tsx
+++ components/shared/products-group-list.tsx
@@ -35,7 +35,7 @@ export const ProductsGroupList: React.FC<Props> = ({
     <div className={cn('mb-10', className)} id={title} ref={intersectionRef}>
       <Title text={title} size="lg" className="font-extrabold mb-5" />
       <div className={cn("grid grid-cols-3 gap-[50px]", listClassName)}>
-        {items.map((item, i) => (
+        {items.map((item) => (
           <ProductCard
             id={item.id}
             key={item.id }
