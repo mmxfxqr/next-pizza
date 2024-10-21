3e765a447177926bcaff2d55d8ddbfba6e32dab5
mmxfxqr <holodov.sas@yandex.ru>
2024-10-21 16:18:14 +0300

    add lib for get all details about pizza, finished design cart-drawer-item, test-output all items in cart

--- shared/components/shared/cart-drawer.tsx
+++ shared/components/shared/cart-drawer.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import {
   Sheet,
@@ -11,6 +12,8 @@ import {
 import { ArrowRight } from "lucide-react";
 import { Button } from "../ui";
 import Link from "next/link";
+import { CartDrawerItem } from "./cart-drawer-item";
+import { getCartItemDetails } from "@/shared/lib";
 interface Props {
   className?: string;
 }
@@ -23,13 +26,99 @@ export const CartDrawer: React.FC<React.PropsWithChildren<Props>> = ({
     <div className={className}>
       <Sheet>
         <SheetTrigger asChild>{children}</SheetTrigger>
-        <SheetContent className="flex flex-col justify-between pb-0 bg-[#ffe1e1]">
+        <SheetContent className="flex flex-col justify-between pb-0 bg-[#f8eaea]">
           <SheetHeader>
             <SheetTitle>
               В корзине <span className="font-bold">3 товара</span>
             </SheetTitle>
           </SheetHeader>
-          {/* Items */}
+          <div className="-mx-6 mt-5 overflow-auto flex-1">
+          <div className="mb-2">
+          <CartDrawerItem
+            id={1}
+            imageUrl={
+              "https://media.dodostatic.net/image/r:292x292/11EE7D612FC7B7FCA5BE822752BEE1E5.jpg"
+            }
+            details={getCartItemDetails(1, 20, [])}
+            name={"Пепперони фреш"}
+            price={500}
+            quantity={1}
+          />
+          </div>
+          <div className="mb-2">
+          <CartDrawerItem
+            id={1}
+            imageUrl={
+              "https://media.dodostatic.net/image/r:292x292/11EE7D612FC7B7FCA5BE822752BEE1E5.jpg"
+            }
+            details={getCartItemDetails(1, 20, [])}
+            name={"Пепперони фреш"}
+            price={500}
+            quantity={1}
+          />
+          </div>
+          <div className="mb-2">
+          <CartDrawerItem
+            id={1}
+            imageUrl={
+              "https://media.dodostatic.net/image/r:292x292/11EE7D612FC7B7FCA5BE822752BEE1E5.jpg"
+            }
+            details={getCartItemDetails(1, 20, [])}
+            name={"Пепперони фреш"}
+            price={500}
+            quantity={1}
+          />
+          </div>
+          <div className="mb-2">
+          <CartDrawerItem
+            id={1}
+            imageUrl={
+              "https://media.dodostatic.net/image/r:292x292/11EE7D612FC7B7FCA5BE822752BEE1E5.jpg"
+            }
+            details={getCartItemDetails(1, 20, [])}
+            name={"Пепперони фреш"}
+            price={500}
+            quantity={1}
+          />
+          </div>
+          <div className="mb-2">
+          <CartDrawerItem
+            id={1}
+            imageUrl={
+              "https://media.dodostatic.net/image/r:292x292/11EE7D612FC7B7FCA5BE822752BEE1E5.jpg"
+            }
+            details={getCartItemDetails(1, 20, [])}
+            name={"Пепперони фреш"}
+            price={500}
+            quantity={1}
+          />
+          </div>
+          <div className="mb-2">
+          <CartDrawerItem
+            id={1}
+            imageUrl={
+              "https://media.dodostatic.net/image/r:292x292/11EE7D612FC7B7FCA5BE822752BEE1E5.jpg"
+            }
+            details={getCartItemDetails(1, 20, [])}
+            name={"Пепперони фреш"}
+            price={500}
+            quantity={1}
+          />
+          </div>
+          <div className="mb-2">
+          <CartDrawerItem
+            id={1}
+            imageUrl={
+              "https://media.dodostatic.net/image/r:292x292/11EE7D612FC7B7FCA5BE822752BEE1E5.jpg"
+            }
+            details={getCartItemDetails(1, 20, [])}
+            name={"Пепперони фреш"}
+            price={500}
+            quantity={1}
+          />
+          </div>
+          
+          </div>
 
           <SheetFooter className="-mx-6 bg-white p-8">
             <div className="w-full">
