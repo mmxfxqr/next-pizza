4140c1921cc5b405e8db3f7f86280714206525a3
mmxfxqr <holodov.sas@yandex.ru>
2024-11-21 17:12:56 +0300

    add auth with login and password

---
+++ shared/components/shared/modals/auth-modal/forms/schemas.ts
@@ -0,0 +1,16 @@
+import {z} from "zod";
+export const passwordSchema = z.string().min(6, {message: "Пароль должен содержать не менее 6 символов"});
+export const formLoginSchema = z.object({
+    email: z.string().email({message: "Введите корректную почту"}),
+    password: passwordSchema,
+});
+export const formRegisterSchema = formLoginSchema.merge(
+    z.object({
+    fullname: z.string().min(2, {message: "Введите корректное имя"}),
+    confirmPassword: passwordSchema,
+})).refine((data) => data.password === data.confirmPassword, {
+    message: "Пароли не совпадают",
+    path: ["confirmPassword"],
+})
+export type FormRegisterSchema = z.infer<typeof formRegisterSchema>
+export type FormLoginSchema = z.infer<typeof formLoginSchema>
