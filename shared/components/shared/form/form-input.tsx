a588d273346da4795f13870b28dc1d4c36c04165
mmxfxqr <holodov.sas@yandex.ru>
2024-11-26 14:02:04 +0300

    fix visual bag with show and delete content of password form field

--- shared/components/shared/form/form-input.tsx
+++ shared/components/shared/form/form-input.tsx
@@ -27,6 +27,7 @@ export const FormInput: React.FC<Props> = ({
   } = useFormContext();
   const value = watch(name);
   const errorText = errors[name]?.message as string;
+  const isPasswordField = props.type === "password" || props.type === "confirmPassword";
   const onClickClear = () => {
     setValue(name, "", { shouldValidate: true });
   };
@@ -43,7 +44,8 @@ export const FormInput: React.FC<Props> = ({
           <Input className="h-12 text-md" {...register(name)} {...props} />
         
 
-        {value && <ClearButton onClick={onClickClear} />}
+        {/* {isPasswordField? value && <ClearButton onClick={onClickClear} className="right-10" /> : value && <ClearButton onClick={onClickClear} />} */}
+        {value && <ClearButton onClick={onClickClear} className={isPasswordField ? "right-10" : ""} />} 
       </div>
       {errorText && <ErrorText text={errorText} className="mt-2" />}
     </div>
