ec739f0516357fa70fdceb1b6974e692dcb84115
mmxfxqr <holodov.sas@yandex.ru>
2024-11-29 06:46:32 +0300

    delete google auth

--- shared/components/shared/modals/auth-modal/auth-modal.tsx
+++ shared/components/shared/modals/auth-modal/auth-modal.tsx
@@ -42,23 +42,6 @@ export const AuthModal: React.FC<Props> = ({ open, onClose }) => {
             />
             GitHub
           </Button>
-          <Button
-            variant="secondary"
-            onClick={() =>
-              signIn("google", {
-                callbackUrl: "/",
-                redirect: true,
-              })
-            }
-            type="button"
-            className="gap-2 h-12 p-2 flex-1"
-          >
-            <img
-              className="w-6 h-6"
-              src="https://fonts.gstatic.com/s/i/productlogos/googleg/v6/24px.svg"
-            />
-            Google
-          </Button>
         </div>
         <Button
           variant="outline"
